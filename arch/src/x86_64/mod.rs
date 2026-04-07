6b0c58dd911d02c608433fbf2680c457edff48ac
Ruben Hakobyan <hruben@meta.com>
2026-04-07 08:05:30 -0700

    arch, hypervisor, vmm: skip vcpu setup when using igvm and kvm
    When we use igvm + kvm, we setup the regs and sregs using the cpuid
    page. We still need to setup the fpu in configure_vcpu.
    
    Co-authored-by: Keith Adler <kadler@cloudflare.com>
    Signed-off-by: Keith Adler <kadler@cloudflare.com>
    Co-authored-by: Alex Orozco <aorozco@google.com>
    Signed-off-by: Alex Orozco <aorozco@google.com>
    Signed-off-by: Ruben Hakobyan <hruben@meta.com>

--- arch/src/x86_64/mod.rs
+++ arch/src/x86_64/mod.rs
@@ -819,6 +819,7 @@ pub fn configure_vcpu(
     cpu_vendor: CpuVendor,
     topology: (u16, u16, u16, u16),
     nested: bool,
+    setup_registers: bool,
 ) -> super::Result<()> {
     let x2apic_id = get_x2apic_id(id, Some(topology));
 
@@ -891,7 +892,9 @@ pub fn configure_vcpu(
     }
 
     regs::setup_msrs(vcpu).map_err(Error::MsrsConfiguration)?;
-    if let Some((kernel_entry_point, guest_memory)) = boot_setup {
+    if let Some((kernel_entry_point, guest_memory)) = boot_setup
+        && setup_registers
+    {
         regs::setup_regs(vcpu, kernel_entry_point).map_err(Error::RegsConfiguration)?;
         regs::setup_fpu(vcpu).map_err(Error::FpuConfiguration)?;
 
